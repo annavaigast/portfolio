a4743022aa9a0bd2167b6a58bdcbddd50407f167
Anna Vaigast <annavaigast@avaigast2022.local>
2022-01-31 00:22:17 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const app = express();
+
+const nodemailer = require('nodemailer');
+
+const PORT = process.env.PORT || 3000
+
+
+app.use(express.static('public'))
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body)
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'anna.vaigast@gmail.com',
+            pass: 'rdmschool14'
+        }
+    })
+
+    const mailOptions = {
+        from: req.body.email,
+        to: 'anna.vaigast@gmail.com',
+        subject: `Message from Portfolio: ${req.body.subject}`,
+        text: req.body.message
+    }
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error){
+            console.log(error);
+            res.send('error')
+        } else {
+            console.log('Email send: ' + info.response)
+            res.send('success')
+        }
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
