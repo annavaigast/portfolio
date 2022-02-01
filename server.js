31cb6744deef93f1c79ce322d12d09fd5e1d2ba7
Anna Vaigast <annavaigast@avaigast2022.local>
2022-01-31 23:22:52 -0500

    contact form

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ const nodemailer = require('nodemailer');
 const PORT = process.env.PORT || 3000
 
 
-app.use(express.static('public'))
-app.use(express.static('index.html'))
+app.use(express.static('public/assets'))
 app.use(express.json())
 
 app.get('/', (req, res) => {
